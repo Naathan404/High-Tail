b946a24bb3b548c8b25be38515b6c62ded21607e
Nguyen Nguyen <nathannguyen6002@gmail.com>
2026-03-06 18:55:37 +0700

    feat: bouncy mushroom

---
+++ Assets/Scripts/Objects/BounceMushroom.cs
@@ -0,0 +1,22 @@
+using DG.Tweening;
+using UnityEngine;
+
+public class BounceMushroom : MonoBehaviour, IBouncy
+{
+    [SerializeField] private Transform _visual;
+    [SerializeField] private float _bouncyForce;
+    public float BouncyForce => _bouncyForce;
+    private Vector3 _originalScale;
+
+    private void Start()
+    {
+        _originalScale = transform.localScale;
+    }
+
+    public void PlayBounceAnimation()
+    {
+        _visual.transform.DOKill();
+        _visual.transform.DOScale(new Vector3(1.5f, 0.6f), 0.1f);
+        _visual.transform.DOScale(_originalScale, 0.5f).SetEase(Ease.OutElastic);
+    }
+}
