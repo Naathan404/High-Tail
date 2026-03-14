fab5d7af29cbb3d444c33f3adb3627458a82e3ee
Nguyen Nguyen <nathannguyen6002@gmail.com>
2026-03-15 00:32:29 +0700

    refactor: separate class

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -246,37 +246,6 @@ public partial class PlayerController : MonoBehaviour
     }
     #endregion
 
-    // private void On
-    // {
-    // }
-
-    #region Collision
-    private void OnCollisionEnter2D(UnityEngine.Collision2D collision)
-    {        
-        if(collision.gameObject.TryGetComponent<IHarmful>(out IHarmful damager))
-        {
-            ApplyHP(-damager.Damage);
-            ApplyKnockback(damager.Knockback);
-        }
-    }
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if(collision.gameObject.TryGetComponent<IBouncy>(out IBouncy bouncer))
-        {
-            _stateMachine.ChangeState(IdleState);
-            ApplyBounce(bouncer.BouncyForce);
-            bouncer.PlayBounceAnimation();   
-        }
-
-        if(collision.gameObject.TryGetComponent<IPushable>(out IPushable pusher))
-        {
-            _stateMachine.ChangeState(IdleState);
-            ApplyPush(pusher.PushForce, pusher.IsRight);
-            pusher.PlayPushAnimation();
-        }
-    }
-
     private void ApplyBounce(float force)
     {
         Rb.linearVelocity = new Vector2(Rb.linearVelocity.x, force);
@@ -293,7 +262,6 @@ public partial class PlayerController : MonoBehaviour
     {
         Rb.linearVelocity = new Vector2(MoveX * -knockback, knockback);
     }
-    #endregion
 
     #region Flip
     public void CheckFlip(float moveX)
