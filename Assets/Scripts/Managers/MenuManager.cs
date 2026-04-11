96f61e633fccc1cf1969711802dcab318f301c8d
nguyenhun11 <giahungcantho11@gmail.com>
2026-04-12 00:09:53 +0700

    feat: some panel

--- Assets/Scripts/Managers/MenuManager.cs
+++ Assets/Scripts/Managers/MenuManager.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class MenuManager : MonoBehaviour
@@ -18,66 +19,117 @@ public class MenuManager : MonoBehaviour
     [SerializeField] private Button newButton;
     [SerializeField] private Button contButton;
     [SerializeField] private Button setButton;
-    [SerializeField] private Button inforButton;
+    [SerializeField] private Button creditButton;
     [SerializeField] private Button exitButton;
     [Header("Navigation")]
     private List<Button> sidePanelButtons = new List<Button>();
     [SerializeField] private int currentButtonIndex = 0;
     private bool isSidePanelOpen = false;
+    [Header("Panels")]
+    [SerializeField] private GameObject settingsPanel;
+    [SerializeField] private GameObject creditPanel;
+    [SerializeField] private GameObject continuePanel;
+    [SerializeField] private GameObject onGamePanel;
 
-    void Start()
+    private void Awake()
     {
         if (sidePanel != null)
         {
-            sidePanelCanvasGroup = sidePanel.GetComponent<CanvasGroup>();
-            if (sidePanelCanvasGroup == null)
+            if (!sidePanel.TryGetComponent<CanvasGroup>(out sidePanelCanvasGroup))
             {
                 sidePanelCanvasGroup = sidePanel.AddComponent<CanvasGroup>();
             }
-            sidePanelCanvasGroup.alpha = 0f;
-            sidePanel.SetActive(false);
         }
+    }
+
+    void Start()
+    {
+        InitializeUI();
+        SetupButtonListeners();
+    }
+
+    private void InitializeUI()
+    {
+        settingsPanel.SetActive(false);
+        creditPanel.SetActive(false);
+        continuePanel.SetActive(false);
 
         pauseButton.gameObject.SetActive(true);
-        pauseButton.onClick.AddListener(() =>
-        {
-            OpenSideMenu(true);
-            pauseButton.gameObject.SetActive(false);
-        });
-
         backButton.gameObject.SetActive(true);
-        backButton.onClick.AddListener(() =>
-        {
-            OpenSideMenu(false);
-        });
 
-        sidePanelButtons.AddRange(new Button[] { newButton, contButton, setButton, inforButton, exitButton });
+        sidePanelButtons = new List<Button> { contButton, newButton, setButton, creditButton, exitButton };
 
-        newButton.onClick.AddListener(() =>
-        {
-            OpenSideMenu(false);
-        });
-        contButton.onClick.AddListener(() =>
-        {
-            OpenSideMenu(false);
-        });
-        setButton.onClick.AddListener(() =>
-        {
-            OpenSideMenu(false);
-        });
-        inforButton.onClick.AddListener(() =>
-        {
-            OpenSideMenu(false);
-        });
-        exitButton.onClick.AddListener(() =>
-        {
-            OpenSideMenu(false);
-        });
+        OpenSideMenu(true, true);
     }
+
+    private void SetupButtonListeners()
+    {
+        pauseButton.onClick.AddListener(OnPauseClicked);
+        backButton.onClick.AddListener(OnBackClicked);
+        newButton.onClick.AddListener(OnNewGameClicked);
+        contButton.onClick.AddListener(OnContinueClicked);
+        setButton.onClick.AddListener(OnSettingsClicked);
+        creditButton.onClick.AddListener(OnCreditClicked);
+        exitButton.onClick.AddListener(OnExitClicked);
+    }
+
+    #region Click Handlers
+    private void OnPauseClicked()
+    {
+        OpenSideMenu(true);
+        ClosePanel();
+        pauseButton.gameObject.SetActive(false);
+    }
+
+    private void OnBackClicked()
+    {
+        OpenSideMenu(false);
+    }
+
+    private void OnNewGameClicked()
+    {
+        OpenSideMenu(false);
+        SceneManager.LoadScene("[OFF] SCENE_1");
+    }
+
+    private void OnContinueClicked()
+    {
+        OpenSideMenu(false);
+        //TODO: Load last checkpoint or save data
+        Debug.Log("Continue game");
+    }
+
+    private void OnSettingsClicked()
+    {
+        OpenSideMenu(false);
+        OpenPanel(settingsPanel, true);
+    }
+
+    private void OnCreditClicked()
+    {
+        OpenSideMenu(false);
+        OpenPanel(creditPanel, true);
+    }
+
+    private void OnExitClicked()
+    {
+        OpenSideMenu(false);
+        //TODO: Show confirmation dialog before quitting
+        Application.Quit();
+    }
+    #endregion
 
     void Update()
     {
         if (!isSidePanelOpen || sidePanelButtons.Count == 0) return;
+        if (Mouse.current != null && Mouse.current.delta.ReadValue().sqrMagnitude > 0.1f)
+        {
+            if (currentButtonIndex != -1)
+            {
+                currentButtonIndex = -1;
+                EventSystem.current.SetSelectedGameObject(null);
+            }
+        }
         if (Keyboard.current == null) return;
         if (/*PauseGameManager.IsGamePaused*/ true)
         {
@@ -89,21 +141,22 @@ public class MenuManager : MonoBehaviour
             {
                 SelectNext();
             }
-            if (Keyboard.current.enterKey.wasPressedThisFrame || Keyboard.current.spaceKey.wasPressedThisFrame)
+            if (currentButtonIndex != -1 && 
+                Keyboard.current.enterKey.wasPressedThisFrame || Keyboard.current.spaceKey.wasPressedThisFrame)
             {
                 sidePanelButtons[currentButtonIndex].onClick.Invoke();
             }
         }
 
     }
-    public void OpenSideMenu(bool open)
+    public void OpenSideMenu(bool open, bool reset = false)
     {
         isSidePanelOpen = open;
         if (open)
         {
-            currentButtonIndex = 0;
-            UpdateSelectionUI();
             ResetButtonScales();
+            UpdateSelectionUI();
+            if (reset) ResetSelection();
             sidePanel.SetActive(true);
             //PauseGameManager.SetPause(true);
             StartCoroutine(DoFade(sidePanelCanvasGroup, 0f, 1f));
@@ -116,6 +169,42 @@ public class MenuManager : MonoBehaviour
                 sidePanel.SetActive(false);
                 //PauseGameManager.SetPause(false);
             }));
+        }
+    }
+
+    private void OpenPanel(GameObject panel, bool open)
+    {
+        if (open)
+        {
+            panel.SetActive(true);
+            StartCoroutine(DoFade(panel.GetComponent<CanvasGroup>(), 0f, 1f));
+        }
+        else
+        {
+            StartCoroutine(DoFade(panel.GetComponent<CanvasGroup>(), 1f, 0f, () =>
+            {
+                panel.SetActive(false);
+            }));
+        }
+    }
+
+    private void ClosePanel(GameObject panel = null)
+    {
+        if (panel != null)
+        {
+            StartCoroutine(DoFade(panel.GetComponent<CanvasGroup>(), 1f, 0f, () =>
+            {
+                panel.SetActive(false);
+            }));
+        }
+        else
+        {
+            if (settingsPanel.activeSelf)
+                OpenPanel(settingsPanel, false);
+            if (creditPanel.activeSelf)
+                OpenPanel(creditPanel, false);
+            if (continuePanel.activeSelf)
+                OpenPanel(continuePanel, false);
         }
     }
 
@@ -133,6 +222,7 @@ public class MenuManager : MonoBehaviour
         onComplete?.Invoke();
     }
 
+    #region Side Panel Button Effects
     private void ResetButtonScales()
     {
         if (sidePanelButtons == null) return;
@@ -141,29 +231,57 @@ public class MenuManager : MonoBehaviour
             btn.transform.localScale = Vector3.one;
         }
     }
+    public void ResetSelection()
+    {
+        currentButtonIndex = -1;
+        EventSystem.current.SetSelectedGameObject(null);
+        ResetButtonScales();
+    }
 
     private void SelectPrevious()
     {
-        currentButtonIndex--;
-        if (currentButtonIndex < 0)
+        if (sidePanelButtons == null || sidePanelButtons.Count == 0) return;
+
+        if (currentButtonIndex == -1)
         {
             currentButtonIndex = sidePanelButtons.Count - 1;
+        }
+        else
+        {
+            currentButtonIndex--;
+            if (currentButtonIndex < 0)
+            {
+                currentButtonIndex = sidePanelButtons.Count - 1;
+            }
         }
         UpdateSelectionUI();
     }
 
     private void SelectNext()
     {
-        currentButtonIndex++;
-        if (currentButtonIndex >= sidePanelButtons.Count)
+        if (sidePanelButtons == null || sidePanelButtons.Count == 0) return;
+
+        if (currentButtonIndex == -1)
         {
             currentButtonIndex = 0;
+        }
+        else
+        {
+            currentButtonIndex++;
+            if (currentButtonIndex >= sidePanelButtons.Count)
+            {
+                currentButtonIndex = 0;
+            }
         }
         UpdateSelectionUI();
     }
 
     private void UpdateSelectionUI()
     {
-        EventSystem.current.SetSelectedGameObject(sidePanelButtons[currentButtonIndex].gameObject);
+        if (currentButtonIndex >= 0 && currentButtonIndex < sidePanelButtons.Count)
+        {
+            EventSystem.current.SetSelectedGameObject(sidePanelButtons[currentButtonIndex].gameObject);
+        }
     }
+    #endregion
 }
