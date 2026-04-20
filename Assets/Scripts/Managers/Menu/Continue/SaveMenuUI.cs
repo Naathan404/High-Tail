e8d735543184a1cc119fede801c540adfc00a2c2
nguyenhun11 <giahungcantho11@gmail.com>
2026-04-21 02:18:37 +0700

    fix: no game saved

--- Assets/Scripts/Managers/Menu/Continue/SaveMenuUI.cs
+++ Assets/Scripts/Managers/Menu/Continue/SaveMenuUI.cs
@@ -1,11 +1,13 @@
 using UnityEngine;
 using System.Collections.Generic;
+using System;
 
 public class SaveMenuUI : MonoBehaviour
 {
     [Header("UI References")]
     [SerializeField] private Transform _contentContainer;
-    [SerializeField] private SaveSlotUI _slotPrefab; //TODO tạo prefabs
+    [SerializeField] private SaveSlotUI _slotPrefab;
+    [SerializeField] private GameObject _emptyDataText;
 
     private void OnEnable()
     {
@@ -19,13 +21,21 @@ public class SaveMenuUI : MonoBehaviour
             Destroy(child.gameObject);
         }
 
-        if (SaveManager.Instance.MainData == null || SaveManager.Instance.MainData.allCommits.Count == 0)
+        if (SaveManager.Instance.MainData == null || SaveManager.Instance.MainData.allCommits == null)
         {
-            // TODO: Hiển thị dòng chữ "Chưa có dữ liệu" trên UI
+            _emptyDataText.SetActive(true);
             return;
         }
 
-        List<SaveNode> history = new List<SaveNode>(SaveManager.Instance.MainData.allCommits);
+        List<SaveNode> history = SaveManager.Instance.MainData.
+            allCommits.FindAll(node => !string.IsNullOrEmpty(node.parentNodeID));
+        Debug.Log($"Lịch sử commit có {history.Count} node");
+        if (history.Count == 0)
+        {
+            _emptyDataText.SetActive(true);
+            return;
+        }
+        _emptyDataText.SetActive(false);
         history.Reverse();
 
         foreach (SaveNode node in history)
