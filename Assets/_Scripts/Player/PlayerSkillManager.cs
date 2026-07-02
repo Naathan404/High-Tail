5428c5ed2b4d3225539a31ab8970c0c948916bd7
Nguyen Nguyen <nathannguyen6002@gmail.com>
2026-07-03 01:04:15 +0700

    fix: tale stone data save load & loading scene sequence

--- Assets/_Scripts/Player/PlayerSkillManager.cs
+++ Assets/_Scripts/Player/PlayerSkillManager.cs
@@ -24,9 +24,6 @@ public partial class PlayerController : MonoBehaviour
     [SerializeField] private LocalizedString _astralPulseName;
     [SerializeField] private LocalizedString _astralPulseDes;
 
-    [SerializeField] private LocalizedString _grabName;
-    [SerializeField] private LocalizedString _grabDes;
-
     [Header("Skill Key Bindings")]
     [SerializeField] private InputActionReference _wallJumpActionRef;
     [SerializeField] private InputActionReference _dashActionRef;
@@ -36,7 +33,7 @@ public partial class PlayerController : MonoBehaviour
     [SerializeField] private InputActionReference _astralPulseActionRef;
     [SerializeField] private InputActionReference _grabActionRef;
 
-    [HideInInspector] public bool[] PlayerSkillUnlockStatus = new bool[7];
+    public bool[] PlayerSkillUnlockStatus = new bool[7];
 
 
     public void UnlockWallJump()
@@ -83,11 +80,5 @@ public partial class PlayerController : MonoBehaviour
     {
         Data.AstralPulseUnlocked = true;
         UIManager.Instance.ShowSkillUnlocked(_astralPulseName, _astralPulseDes, _astralPulseActionRef);
-    }
-
-    public void UnlockGrab()
-    {
-        Data.GrabUnlocked = true;
-        UIManager.Instance.ShowSkillUnlocked(_grabName, _grabDes, _grabActionRef);
     }
 }
